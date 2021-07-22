8699e7109ea6a1643a5848daf127b83fac8e5c03
lightwire <mintael@gmail.com>
2021-07-22 12:00:53 +0900

    Splah module update

--- app/src/main/java/com/example/tpictest/IntermediateActivity.java
+++ app/src/main/java/com/example/tpictest/IntermediateActivity.java
@@ -3,27 +3,20 @@ package com.example.tpictest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class IntermediateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(false);
-        }
-        setContentView(R.layout.activity_splash);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        setContentView(R.layout.activity_blank);
 
-//            PreferenceSetting preferenceSetting = new PreferenceSetting(getBaseContext());
         getWindow().getDecorView().postDelayed(() -> {
-            Intent intent = new Intent(getBaseContext(), IntermediateActivity.class);
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-        }, 3000);
-
+        }, 2000);
     }
 }
