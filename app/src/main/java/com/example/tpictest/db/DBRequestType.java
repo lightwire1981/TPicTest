8cda2389aad5f3a97bb2f307864e78ff836b57a8
LEEMINTAE <lightwire@soulsoft.co.kr>
2021-08-14 01:51:56 +0900

    Database Code Update

---
+++ app/src/main/java/com/example/tpictest/db/DBRequestType.java
@@ -0,0 +1,8 @@
+package com.example.tpictest.db;
+
+public enum DBRequestType {
+    JOIN,
+    CREATE_KID,
+    LIKE,
+    WRITE_REVIEW
+}
--- app/src/main/java/com/example/tpictest/db/DatabaseRequest.java
+++ app/src/main/java/com/example/tpictest/db/DatabaseRequest.java
@@ -6,6 +6,15 @@ import android.util.Log;
 
 import com.example.tpictest.utils.PreferenceSetting;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class DatabaseRequest extends AsyncTask<String, String, String> {
 
     private static String SERVER_IP;
@@ -17,14 +26,7 @@ public class DatabaseRequest extends AsyncTask<String, String, String> {
     private final ExecuteListener executeListener;
 
     private final String USE = "USE=";
-    private String dbUse;
-
-    public enum REQUEST_TYPE {
-        JOIN,
-        CREATE_KID,
-        LIKE,
-        WRITE_REVIEW
-    }
+    private DBRequestType requestType;
 
     private String TAG = "DatabaseRequest";
 
@@ -37,22 +39,87 @@ public class DatabaseRequest extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... params) {
         String response;
-        String parameters;
-        dbUse = params[0];
+        String parameters = "";
+        String dbUse = params[0];
 
-        switch (dbUse) {
-            case "JOIN":
+        requestType = DBRequestType.valueOf(dbUse);
+
+        switch (requestType) {
+            case JOIN:
+                parameters = MakeParameter(requestType, params);
+                Log.i(TAG, "Parameter Check ::" + parameters);
+                break;
+            case CREATE_KID:
+                break;
+            case LIKE:
+                break;
+            case WRITE_REVIEW:
                 break;
             default:
                 break;
         }
 
+        try {
+            HttpURLConnection httpURLConnection = (HttpURLConnection)(new URL("http://"+SERVER_IP+PHP_URL)).openConnection();
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setUseCaches(false);
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-        return null;
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(httpURLConnection.getOutputStream(), StandardCharsets.UTF_8);
+            PrintWriter printWriter = new PrintWriter(outputStreamWriter);
+            printWriter.write(parameters);
+            printWriter.flush();
+
+            if (httpURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                Log.e(TAG, "Http Connection Fail");
+                return null;
+            }
+            response = (new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), StandardCharsets.UTF_8))).readLine();
+            Log.d(TAG, "Response Value :: "+response);
+
+            printWriter.close();
+            outputStreamWriter.close();
+
+        } catch (IOException e) {
+            Log.e("Call at : "+e.getStackTrace()[1].getClassName()+" "+
+                    e.getStackTrace()[1].getMethodName(), "Exception Occur");
+            return null;
+        }
+        return response;
     }
 
     @Override
     protected void onPostExecute(String response) {
+        switch (requestType) {
+            case JOIN:
+                Log.d(TAG, response);
+                executeListener.onResult(response);
+                break;
+            case CREATE_KID:
+                break;
+            case LIKE:
+                break;
+            case WRITE_REVIEW:
+                break;
+            default:
+                break;
+        }
         super.onPostExecute(response);
+    }
+
+    private String MakeParameter(DBRequestType type, String... params) {
+
+        switch (type) {
+            case JOIN:
+                break;
+            default:
+                break;
+        }
+
+        return null;
     }
 }
