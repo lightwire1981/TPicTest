2d34e262874ae852c2a9837b97d2b4103897ac47
LEEMINTAE <lightwire@soulsoft.co.kr>
2021-08-18 03:42:52 +0900

    Child Character Select Update

--- app/src/main/java/com/example/tpictest/fragments/CharacterSelectFragment.java
+++ app/src/main/java/com/example/tpictest/fragments/CharacterSelectFragment.java
@@ -1,14 +1,26 @@
 package com.example.tpictest.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tpictest.R;
+import com.example.tpictest.list_code.ListAdapterCharSelect;
+import com.example.tpictest.list_code.ListItemCharSelect;
+import com.example.tpictest.list_code.RecyclerDecoration;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +73,137 @@ public class CharacterSelectFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_select, container, false);
+        View view = inflater.inflate(R.layout.fragment_character_select, container, false);
+
+        RecyclerView characterList = view.findViewById(R.id.rcyclVwCharacterList);
+//        setCharacterList(characterList);
+        setCharacterListTemp(characterList);
+        return view;
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private void setCharacterListTemp(RecyclerView recyclerView) {
+        ArrayList<ListItemCharSelect> mList = new ArrayList<>();
+        String[] name = getResources().getStringArray(R.array.txt_kids_char);
+
+        // 캐릭터 11개 가정하여 셋팅
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            ListItemCharSelect item = new ListItemCharSelect();
+            item.setItemCount(3);
+            item.setCharDrawable1(requireContext().getDrawable(R.drawable.tp_icon_brand09_off));
+            item.setCharName1(name[index]);
+            index++;
+            item.setCharDrawable2(requireContext().getDrawable(R.drawable.tp_icon_brand09_off));
+            item.setCharName2(name[index]);
+            index++;
+            item.setCharDrawable2(requireContext().getDrawable(R.drawable.tp_icon_brand09_off));
+            item.setCharName2(name[index]);
+            index++;
+            mList.add(item);
+        }
+        ListItemCharSelect item = new ListItemCharSelect();
+        item.setItemCount(2);
+        item.setCharDrawable1(requireContext().getDrawable(R.drawable.tp_icon_brand09_off));
+        item.setCharName1(name[index]);
+        index++;
+        item.setCharDrawable2(requireContext().getDrawable(R.drawable.tp_icon_brand09_off));
+        item.setCharName2(name[index]);
+        mList.add(item);
+
+        recyclerView.setAdapter(new ListAdapterCharSelect(mList));
+        setLayoutManager(recyclerView);
+    }
+
+    private void setCharacterList(RecyclerView recyclerView) {
+        ArrayList<ListItemCharSelect> mList = new ArrayList<>();
+
+        JSONArray jsonArray = getCharacterData();
+        int count = jsonArray.length();
+
+        int quotient = count/3;
+        int remainder = count%3;
+
+        if (quotient < 1) {
+            for (int index=0; index < jsonArray.length(); index++) {
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(index);
+                    ListItemCharSelect item = new ListItemCharSelect();
+                    item.setItemCount(count);
+                    // do ListItem work
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            int index = 0;
+            for (int x = 0; x < quotient; x++) {
+                ListItemCharSelect item = new ListItemCharSelect();
+                item.setItemCount(3);
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(index);
+                    // do ListItem work
+                    index++;
+                    jsonObject = jsonArray.getJSONObject(index);
+                    // do ListItem work
+                    index++;
+                    jsonObject = jsonArray.getJSONObject(index);
+                    // do ListItem work
+                    index++;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                mList.add(item);
+            }
+
+            // after quotient work index is full of 3x value -> ex) 3, 6, 9, 12 ...
+            // remainder is only 0 or 1 or 2
+            ListItemCharSelect item = new ListItemCharSelect();
+            switch (remainder) {
+                case 1:
+                    item.setItemCount(1);
+                    try {
+                        JSONObject jsonObject = jsonArray.getJSONObject(index);
+                        // do ListItem work
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    mList.add(item);
+                    break;
+                case 2:
+                    item.setItemCount(2);
+                    try {
+                        JSONObject jsonObject = jsonArray.getJSONObject(index);
+                        // do ListItem work
+                        index++;
+                        jsonObject = jsonArray.getJSONObject(index);
+                        // do ListItem work
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    mList.add(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private JSONArray getCharacterData() {
+
+        // do Database Work;
+
+        return new JSONArray();
+    }
+
+
+    private void setLayoutManager(RecyclerView recyclerView) {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.addItemDecoration(new RecyclerDecoration(0, 25));
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 }
