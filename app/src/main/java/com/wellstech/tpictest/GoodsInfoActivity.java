f9eb94e05ad3e7bf35b6aaa16fdd44a3a7e07221
LEEMINTAE <lightwire@soulsoft.co.kr>
2021-08-29 04:11:37 +0900

    Goods Info Activity update

---
+++ app/src/main/java/com/wellstech/tpictest/GoodsInfoActivity.java
@@ -0,0 +1,13 @@
+package com.wellstech.tpictest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoodsInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_info);
+    }
+}
