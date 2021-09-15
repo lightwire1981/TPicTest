775049c2100c3767554bd4760bc3f6cd96b997d3
LEEMINTAE <lightwire@soulsoft.co.kr>
2021-09-16 01:22:29 +0900

    Main Page Update

--- app/src/main/java/com/auroraworld/toypic/list_code/ListAdapterCustomToy.java
+++ app/src/main/java/com/auroraworld/toypic/list_code/ListAdapterCustomToy.java
@@ -1,30 +1,36 @@
 package com.auroraworld.toypic.list_code;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.auroraworld.toypic.GoodsInfoActivity;
 import com.auroraworld.toypic.R;
+import com.auroraworld.toypic.db.DBRequestType;
+import com.auroraworld.toypic.db.DatabaseRequest;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class ListAdapterCustomToy extends RecyclerView.Adapter<ListAdapterCustomToy.ViewHolder> {
     private Context context;
     private final ArrayList<ListItemCustomToy> mData;
+    private final String TAG = getClass().getSimpleName();
 
     public ListAdapterCustomToy(ArrayList<ListItemCustomToy> list) {
         mData = list;
@@ -43,25 +49,53 @@ public class ListAdapterCustomToy extends RecyclerView.Adapter<ListAdapterCustom
     public void onBindViewHolder(@NonNull @NotNull ListAdapterCustomToy.ViewHolder holder, int position) {
         ListItemCustomToy item = mData.get(position);
 
-//        holder.productImg.setImageDrawable(item.getImgDrawable());
         Glide.with(context).
                 load(item.getGoodsImgUrl()).
                 placeholder(R.drawable.tp_icon_brand01_on).
                 diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).
                 into(holder.goodsImg);
-        holder.goodsImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+        holder.goodsImg.setOnClickListener(view -> {
+            JSONObject goodsData = new JSONObject();
+            try {
+                goodsData.put("goodsNo", item.getGoodsId());
+                new DatabaseRequest(context, result -> {
+                    if (result[0].equals("null")) {
+                        Toast.makeText(context, "상품데이터가 없습니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Intent intent = new Intent(context, GoodsInfoActivity.class);
+                    intent.putExtra("goodsInfo", result[0]);
+                    context.startActivity(intent);
+                }).execute(DBRequestType.GET_GOODS_INFO.name(), goodsData.toString());
+                Log.i(TAG, goodsData.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         });
-        holder.goodsLike.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-            }
+        holder.goodsLike.setOnCheckedChangeListener((compoundButton, b) -> {
+
         });
+
         holder.goodsName.setText(item.getGoodsName());
+        holder.goodsName.setOnClickListener(view -> {
+            JSONObject goodsData = new JSONObject();
+            try {
+                goodsData.put("goodsNo", item.getGoodsId());
+                new DatabaseRequest(context, result -> {
+                    if (result[0].equals("null")) {
+                        Toast.makeText(context, "상품데이터가 없습니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Intent intent = new Intent(context, GoodsInfoActivity.class);
+                    intent.putExtra("goodsInfo", result[0]);
+                    context.startActivity(intent);
+                }).execute(DBRequestType.GET_GOODS_INFO.name(), goodsData.toString());
+                Log.i(TAG, goodsData.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
         holder.goodsRate.setText(item.getGoodsRate());
 //        if (item.isFavorite()) {
 //
